38068a5e7e3be9827249150feba647c77b61ce32
qiuy <775296271@qq.com>
2023-04-21 12:27:03 +0800

    first commit

---
+++ src/views/NotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="not-found">
+    <el-empty
+      description="404 Not Found"
+      :image-size="400"
+      :image="imgUrl"
+    >
+      <el-button @click="$router.back()">返回上一页</el-button>
+    </el-empty>
+  </div>
+</template>
+
+<script>
+let imgUrl =require('@/assets/img/404.svg')
+console.log(imgUrl);
+export default {
+  data() {
+    return {
+      imgUrl
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.not-found {
+  height: 100vh;
+}
+</style>
